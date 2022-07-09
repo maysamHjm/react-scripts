b40de10996b938182683e5851ed86e0e34d4c2d9
Maysam Milani <maysam7071@gmail.com>
2022-07-09 10:26:09 +0430

    add rtl

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -87,6 +87,9 @@ const hasJsxRuntime = (() => {
   }
 })();
 
+const postcssRTLCSS = require('postcss-rtlcss');
+const { Source } = require('postcss-rtlcss/options');
+
 // This is the production and development configuration.
 // It is focused on developer experience, fast rebuilds, and a minimal bundle.
 module.exports = function (webpackEnv) {
@@ -132,6 +135,7 @@ module.exports = function (webpackEnv) {
           // https://github.com/facebook/create-react-app/issues/2677
           ident: 'postcss',
           plugins: () => [
+            postcssRTLCSS({source: Source.rtl}),
             require('postcss-flexbugs-fixes'),
             require('postcss-preset-env')({
               autoprefixer: {
